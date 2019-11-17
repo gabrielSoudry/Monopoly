f650e983471a6c322c7fb205eae799e310d5e1c5
Gabi22top <gabriel_s@hotmail.fr>
2019-11-17 21:43:37 +0100

    Build failure test another yaml

---
+++ model/CommunityChestLand.cs
@@ -0,0 +1,10 @@
+﻿using Monopoly_TD7.model.Lands;
+
+namespace Monopoly_TD7.model
+{
+    public class CommunityChestLand : Land
+    {
+        public CommunityChestLand() => (Type) = (LandType.CommunityChest);
+
+    }
+}
